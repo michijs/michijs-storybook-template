4a843796473254279976095695736a6432a830f0
Lucas Segurado <segurado1996@gmail.com>
2021-10-31 19:33:10 -0300

    Better support for theming

--- src/storybook-utils/bind.tsx
+++ src/storybook-utils/bind.tsx
@@ -4,21 +4,29 @@ import { themes } from '@storybook/theming';
 export function bind(Story: JSX.Element) {
   const typedJSXElement = Story as ObjectJSXElement | FunctionJSXElement | FragmentJSXElement | ClassJSXElement
   const auxFunction = (attrs, { globals }) => {
-    const { theme } = globals;
+    const { theme, backgrounds } = globals as {theme: 'dark' | 'light', backgrounds?: {
+      value: '#F8F8F8' | '#333333'
+    }};
+    const storyTheme = backgrounds?.value ? (backgrounds.value === '#333333' ? 'dark' : 'light'): undefined;
+    const finalTheme = storyTheme ?? theme;
+
     typedJSXElement.attrs = { ...typedJSXElement.attrs, theme, ...attrs }
     const style = css`
+      body {
+        padding: 0 !important;
+      }
       html,
       body,
       #root-storybook,
       #root {
         height: 100%;
         margin: 0;
-        color: ${themes[globals.theme].textColor};
-        background-color: ${themes[globals.theme].appBg};
+        color: ${themes[finalTheme].textColor};
+        background-color: ${themes[finalTheme].appBg};
         box-sizing: border-box;
       }
       #root-storybook {
-        padding: 8px;
+        padding: 16px;
       }
     `
     return renderFunctionalComponent(
